4568cd54d19c79dd8cbbec2160207929fab8d21d
Matthias Schmitt <male.schmitt@posteo.de>
2017-12-12 17:05:54 +0100

    Add libsvm format support to csv_flann

--- app/csv_flann.cpp
+++ app/csv_flann.cpp
@@ -13,10 +13,11 @@ using namespace std;
 
 typedef vector<FeatureVec> MyMat;
 
+int parse_args(int argc, char *argv[], int & nn_num, int & label_col, bool & normalize, bool & libsvm, string & inputfile, string & outputfile);
 
-int parse_args(int argc, char *argv[], int & nn_num, int & label_col, bool & normalize, string & inputfile, string & outputfile);
+void read_csv(const string & filename, MyMat & min_data, vector<int> & maj_data, int label_col = 0);
 
-void readCSV(const string filename, MyMat & min_data, vector<int> & maj_data, int label_col = 0);
+void read_libsvm(const string & filename, MyMat & data, vector<int> & label);
 
 void normalize(MyMat & data);
 
@@ -31,22 +32,30 @@ void write_features(const MyMat & data, const string filename);
 int main(int argc, char *argv[]) {
         int nn_num = 10;
         int label_col = 0;
-        bool norm = true; //indicates whether to normalize to [-1,1] or scale to [0,1]
-        //the later will preserve null entries
+        bool norm = true; //indicates whether to normalize or scale to [0,1]
+                          //the later will preserve null entries
+        bool libsvm = false; // read libsvm data instead of csv
         string inputfile;
         string outputfile;
 
-        if (parse_args(argc, argv, nn_num, label_col, norm, inputfile, outputfile))
+        if (parse_args(argc, argv, nn_num, label_col, norm, libsvm, inputfile, outputfile)){
+                cout << "parse_args error. exiting..." << "\n";
                 return 1;
+        }
 
         MyMat data;
         vector<int> label;
 
         timer t;
 
-        readCSV(inputfile, data, label, label_col);
+        if (libsvm) {
+                read_libsvm(inputfile, data, label);
+                cout << "read libsvm time " << t.elapsed() << endl;
+        } else {
+                read_csv(inputfile, data, label, label_col);
+                cout << "read csv time " << t.elapsed() << endl;
+        }
 
-        cout << "read csv time " << t.elapsed() << endl;
 
         size_t rows = data.size();
         size_t cols = data[0].size();
@@ -72,16 +81,22 @@ int main(int argc, char *argv[]) {
 
         cout << "splitting time " << t.elapsed() << endl;
 
+        std::cou << "min nodes " << min_data.size()
+                 << " maj nodes " << maj_data.size();
+
         vector<vector<Edge>> min_edges;
         vector<vector<Edge>> maj_edges;
 
         t.restart();
 
         svm_flann::run_flann(min_data, min_edges, nn_num);
+
+        std::cout << "flann time min " << t.elapsed() << std::endl;
+        t.restart();
+
         svm_flann::run_flann(maj_data, maj_edges, nn_num);
 
-        cout << "flann time " << t.elapsed() << endl;
-
+        cout << "flann time maj " << t.elapsed() << endl;
         t.restart();
 
         write_metis(min_edges, outputfile + "_min_graph");
@@ -94,13 +109,13 @@ int main(int argc, char *argv[]) {
         return 0;
 }
 
-int parse_args(int argc, char *argv[], int & nn_num, int & label_col, bool & normalize, string & inputfile, string & outputfile) {
+int parse_args(int argc, char *argv[], int & nn_num, int & label_col, bool & normalize, bool & libsvm, string & inputfile, string & outputfile) {
         // Setup argtable parameters.
         struct arg_end *end                 = arg_end(100);
         struct arg_lit *help                = arg_lit0("h", "help","Print help.");
         struct arg_int *nearest_neighbors   = arg_int0(NULL, "nn", NULL, "Number of nearest neighbors to compute.");
         struct arg_int *label_column        = arg_int0(NULL, "label_col", NULL, "column in which the labels are written (starting at 0)");
-        struct arg_lit *scale               = arg_lit0(NULL, "scale", "don't normalize to [-1,1] just scale to [0,1]");
+        struct arg_lit *scale               = arg_lit0(NULL, "scale", "don't normalize just scale to [0,1]");
         struct arg_str *filename            = arg_strn(NULL, NULL, "FILE", 1, 1, "Path to csv file to process.");
         struct arg_str *filename_output     = arg_str0("o", "output_filename", "OUTPUT", "Specify the name of the output file. \"path_{min,maj}_{graph,data}\" will be used as output. default: FILE without extension");
 
@@ -135,7 +150,10 @@ int parse_args(int argc, char *argv[], int & nn_num, int & label_col, bool & nor
 
         if (filename->count > 0) {
                 inputfile = filename->sval[0];
-                outputfile = inputfile.substr(0,inputfile.find_last_of('.'));
+                auto last_dot = inputfile.find_last_of('.');
+                if (!last_dot || last_dot == std::string::npos)
+                        libsvm = true;
+                outputfile = inputfile.substr(0, last_dot);
         }
 
         if (filename_output->count > 0) {
@@ -147,7 +165,7 @@ int parse_args(int argc, char *argv[], int & nn_num, int & label_col, bool & nor
         return 0;
 }
 
-void readCSV(const string filename, MyMat & data, vector<int> & label, int label_col) {
+void read_csv(const string & filename, MyMat & data, vector<int> & label, int label_col) {
         ifstream file;
         file.open(filename);
 
@@ -172,6 +190,47 @@ void readCSV(const string filename, MyMat & data, vector<int> & label, int label
                         col++;
                 }
         }
+}
+
+void read_libsvm(const string & filename, MyMat & data, vector<int> & label) {
+        cout << "begin " << filename << endl;
+
+        ifstream file;
+        size_t feature_size = 1;
+        file.open(filename);
+
+        for (string line; getline(file, line); ) {
+                stringstream sep(line);
+                string item;
+
+                data.push_back(FeatureVec());
+                data.back().reserve(feature_size);
+
+                getline(sep, item, ' ');
+                label.push_back(stoi(item));
+
+                for (; getline(sep, item, ' '); ) {
+                        auto colon_pos = item.find(':');
+                        int index = stoi(item.substr(0, colon_pos));
+                        FeatureData value = stod(item.substr(colon_pos+1));
+
+                        while (data.back().size() < index - 1) {
+                                data.back().push_back(0);
+                        }
+
+                        data.back().push_back(value);
+
+                }
+                feature_size = std::max(data.back().size(), feature_size);
+        }
+
+        cout << "resize now" << "\n";
+
+        for (auto&& row : data) {
+                if (row.size() < feature_size)
+                        row.resize(feature_size);
+        }
+        cout << "done" << endl;
 }
 
 void normalize(MyMat & data) {
--- lib/svm/svm_flann.cpp
+++ lib/svm/svm_flann.cpp
@@ -10,16 +10,12 @@ void svm_flann::run_flann(const std::vector<FeatureVec> & data, std::vector<std:
         size_t rows = data.size();
         size_t cols = data[0].size();
 
-        std::cout << "data points: " << rows << " features: " << cols << std::endl;
-
         // copy data to a linear vector, because flann needs a pointer
         FeatureVec tmp;
         tmp.reserve(rows*cols);
         for (size_t i = 0; i < rows; ++i) {
                 tmp.insert(tmp.end(), data[i].begin(), data[i].end());
         }
-
-        timer t;
 
         flann::Matrix<FeatureData> mat(tmp.data(), rows, cols);
         flann::Index<flann::L2<float>> index(mat, flann::KDTreeIndexParams(1));
@@ -36,7 +32,8 @@ void svm_flann::run_flann(const std::vector<FeatureVec> & data, std::vector<std:
         graph.clear();
         graph.reserve(rows);
         for (size_t i = 0; i < rows; ++i) {
-                std::vector<Edge> edges(num_nn);
+                graph.push_back(std::vector<Edge>(num_nn));
+
                 for (size_t j = 0; j < (size_t) num_nn + 1; ++j) {
                         int target = indices[i][j];
                         float weight = 1 / distances[i][j];
@@ -47,10 +44,8 @@ void svm_flann::run_flann(const std::vector<FeatureVec> & data, std::vector<std:
                         Edge edge;
                         edge.target = target;
                         edge.weight = weight;
-                        edges.push_back(edge);
+                        graph.back().push_back(edge);
                 }
-                graph.push_back(std::move(edges));
         }
 
-        std::cout << "build knn " << t.elapsed() << std::endl;
 }
