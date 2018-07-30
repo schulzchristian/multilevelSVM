418f4a065b4b94749bc1ba7490444e6e1746092f
Matthias Schmitt <male.schmitt@posteo.de>
2018-07-30 13:21:26 +0200

    k-fold import add debug output

--- lib/svm/k_fold_import.cpp
+++ lib/svm/k_fold_import.cpp
@@ -31,20 +31,31 @@ void k_fold_import::next_intern(double & io_time) {
         std::vector<FeatureVec> min_features;
         std::vector<std::vector<Edge>> min_edges;
         svm_io::readFeaturesLines(min_train_name, min_features);
+        std::cout << "read " << min_features.size() << " lines" << std::endl;
         io_time += t.elapsed();
         svm_flann::run_flann(min_features, min_edges);
+        std::cout << "ran flann " << min_edges.size() << " edges" << std::endl;
         graph_io::readGraphFromVec(this->cur_min_graph, min_edges, min_features.size() * nn * 2);
+        std::cout << "read graph from vec " << this->cur_min_graph.number_of_nodes() << " nodes " << this->cur_min_graph.number_of_edges() << " edges " << std::endl;
         graph_io::readFeatures(this->cur_min_graph, min_features);
+        std::cout << "read features" << std::endl;
 
+        std::cout << "reading " << maj_train_name << std::endl;
 
         t.restart();
         std::vector<FeatureVec> maj_features;
         std::vector<std::vector<Edge>> maj_edges;
         svm_io::readFeaturesLines(maj_train_name, maj_features);
+        std::cout << "read " << maj_features.size() << " lines" << std::endl;
         io_time += t.elapsed();
         svm_flann::run_flann(maj_features, maj_edges);
+        std::cout << "ran flann " << maj_edges.size() << " edges" << std::endl;
         graph_io::readGraphFromVec(this->cur_maj_graph, maj_edges, maj_features.size() * nn * 2);
+        std::cout << "read graph from vec " << this->cur_maj_graph.number_of_nodes() << " nodes " << this->cur_maj_graph.number_of_edges() << " edges " << std::endl;
         graph_io::readFeatures(this->cur_maj_graph, maj_features);
+        std::cout << "read features" << std::endl;
+
+        std::cout << "reading " << test_name << std::endl;
 
         t.restart();
         svm_io::readTestSplit(test_name, this->cur_min_test, this->cur_maj_test);
