fc55c19da42452c01bf883aaeac87a062c83505e
Matthias Schmitt <male.schmitt@posteo.de>
2018-07-31 21:46:32 +0200

    Add csv_to_libsvm.py

---
+++ tools/convert_libsvm.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+
+
+kfold_p_train_data_exp_1_fold_4_exp_0.1_data
---
+++ tools/csv_to_libsvm.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+
+import sys
+
+label = ""
+
+if len(sys.argv) == 2:
+    label = sys.argv[1]
+
+firstline = True
+for line in sys.stdin:
+    if firstline:
+        firstline = False
+        continue
+
+    processedlabel = False
+
+    if label:
+        print label,
+        processedlabel = True
+
+    idx = 1
+    for word in line.split():
+        if not processedlabel:
+            print word,
+            processedlabel = True
+            continue
+
+        print "{}:{}".format(idx, word),
+        idx += 1
+
+    print
