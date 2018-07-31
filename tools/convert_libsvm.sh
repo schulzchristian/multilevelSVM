f809f3dfd868f7a4fadf0bafa36853b80e2ea42c
Matthias Schmitt <male.schmitt@posteo.de>
2018-08-01 00:18:34 +0200

    Fix libsvm convert script

--- tools/convert_libsvm.sh
+++ tools/convert_libsvm.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 csv_to_libsvm=$HOME/bachelor/multilevelSVM/tools/csv_to_libsvm.py
-orgi_dir=${pwd}
+start_dir=$(pwd)
+echo start_dir \"$start_dir\"
 
-for d in $( find . -name temp -d );
+for d in $( find -L . -name temp -d );
 do
-    echo $d
+    echo dir $d
     cd $d
 
     for e in $( seq 0 4 );
@@ -18,12 +19,11 @@ do
             train_file=train_exp_${e}_kfold_${f}.libsvm
             test_file=test_exp_${e}_kfold_${f}.libsvm
 
-            echo $train_file
-
             $csv_to_libsvm "1" < kfold_p_train_data_exp_${e}_fold_${f}_exp_0.1_data > $train_file
             $csv_to_libsvm "-1" < kfold_n_train_data_exp_${e}_fold_${f}_exp_0.1_data >> $train_file
             $csv_to_libsvm < kfold_test_data_exp_${e}_fold_${f}_exp_0.1_data > $test_file
         done
     done
-    cd $orig_dir
+    echo back to $start_dir
+    cd $start_dir
 done
