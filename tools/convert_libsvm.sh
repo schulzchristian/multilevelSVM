52cf44ec1b86a47cf148e45fd9fe729d27fd37ce
Matthias Schmitt <male.schmitt@posteo.de>
2018-07-31 23:56:43 +0200

    Add tool to convert from csv to libsvm

--- tools/convert_libsvm.sh
+++ tools/convert_libsvm.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
 
+csv_to_libsvm=$HOME/bachelor/multilevelSVM/tools/csv_to_libsvm.py
+orgi_dir=${pwd}
 
+for d in $( find . -name temp -d );
+do
+    echo $d
+    cd $d
 
+    for e in $( seq 0 4 );
+    do
+        echo exp${e}
+        for f in $( seq 0 4 );
+        do
+            echo kfold${f}
 
-kfold_p_train_data_exp_1_fold_4_exp_0.1_data
+            train_file=train_exp_${e}_kfold_${f}.libsvm
+            test_file=test_exp_${e}_kfold_${f}.libsvm
+
+            echo $train_file
+
+            $csv_to_libsvm "1" < kfold_p_train_data_exp_${e}_fold_${f}_exp_0.1_data > $train_file
+            $csv_to_libsvm "-1" < kfold_n_train_data_exp_${e}_fold_${f}_exp_0.1_data >> $train_file
+            $csv_to_libsvm < kfold_test_data_exp_${e}_fold_${f}_exp_0.1_data > $test_file
+        done
+    done
+    cd $orig_dir
+done
